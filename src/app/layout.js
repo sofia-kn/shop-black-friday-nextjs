d28bd2ebec3868f5c660a5bc6d9e54fc9838eb7d
Mehrdad.Pashaei <mehrdad.pashaei1@gmail.com>
2023-12-25 01:09:48 -0800

    feat(root): added components directory and header and footer component for layout.

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Header from "@/app/(components)/header/Header";
+import Footer from "@/app/(components)/footer/Footer";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,7 +13,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+      <nav>
+          <Header/>
+      </nav>
+      {children}
+      <footer>
+          <Footer/>
+      </footer>
+      </body>
     </html>
   )
 }
