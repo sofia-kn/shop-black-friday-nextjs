42b7cced3a99d9e0dfbbda1ff74c605248fc3bcc
Sofia Katiran <sofiakatiran@gmail.com>
2023-12-28 12:24:46 +0330

    feat(global.css & assets): add fonts and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,48 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    screens: {
+      sm: "545px",
+      "sx-max": { max: "545px" },
+      md: "768px",
+      "md-max": { max: "768px" },
+      lg: "920px",
+      "lg-max": { max: "920px" },
+      xl: "1200px",
+      "xl-max": { max: "1200px" },
+      "2xl": "1320px",
+      "2xl-max": { max: "1320px" },
     },
+    colors: ({ colors }) => ({
+      inherit: colors.inherit,
+      current: colors.current,
+      transparent: colors.transparent,
+      black: colors.black,
+      white: colors.white,
+
+      slate: {
+        DEFAULT: colors.slate,
+        50: "#f8fafc",
+        // 100: "#dee2e6",
+        // 200: "#cbd3da",
+        // 300: "#a8b8d8",
+        // 400: "#8392ab",
+        // 500: "#67748e",
+        // 600: "#627594",
+        // 700: "#344767",
+        // 750: "#323a54",
+        // 800: "#3a416f",
+        // 850: "#111c44",
+        // 900: "#051139",
+      },
+    }),
+    plugins: [
+      
+    ],
   },
-  plugins: [],
-}
+};
