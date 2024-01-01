4d3ab6947f4a046ccc62ae574f33b0b7cc2bbf12
Sofia Katiran <sofiakatiran@gmail.com>
2024-01-01 13:58:24 +0330

    feat(page): fixed timer bugs and styled new section

--- src/app/page.js
+++ src/app/page.js
@@ -1,66 +1,90 @@
 "use client";
-import React, { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
+import Image from "next/image";
 
 export default function Home() {
-  const [timerDays, setTimerDays] = useState("00");
-  const [timerHours, setTimerHours] = useState("00");
-  const [timerMinutes, setTimerMinutes] = useState("00");
-  const [timerSeconds, setTimerSeconds] = useState("00");
-  let interval = useRef();
+  const [partyTime, setPartyTime] = useState(false);
+  const [timerDays, setTimerDays] = useState(0);
+  const [timerHours, setTimerHours] = useState(0);
+  const [timerMinutes, setTimerMinutes] = useState(0);
+  const [timerSeconds, setTimerSeconds] = useState(0);
 
-  const startTimer = () => {
-    const countdownDate = new Date("May 30, 2020 00:00:00").getTime();
+  useEffect(() => {
+    const target = new Date("01/04/2024 18:24:00");
 
-    interval = setInterval(() => {
-      const now = new Date().getTime();
-      const distance = countdownDate - now;
+    const interval = setInterval(() => {
+      const now = new Date();
+      const difference = target.getTime() - now.getTime();
+      const d = Math.floor(difference / (1000 * 60 * 60 * 24));
+      setTimerDays(d);
 
-      const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-      const hours = Math.floor(
-        (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      const h = Math.floor(
+        (difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
       );
-      const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-      const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+      setTimerHours(h);
 
-      if (distance < 0) {
-        //stop our timer
-        clearInterval(interval.current);
-      } else {
-        //update our timer
-        setTimerDays(days);
-        setTimerHours(hours);
-        setTimerMinutes(minutes);
-        setTimerSeconds(seconds);
+      const m = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+      setTimerMinutes(m);
+
+      const s = Math.floor((difference % (1000 * 60)) / 1000);
+      setTimerSeconds(s);
+
+      if (d <= 0 && h <= 0 && m <= 0 && s <= 0) {
+        setPartyTime(true);
       }
     }, 1000);
-    
-  };
-  console.log(timerDays);
-  //componentDidMount
-  useEffect(() => {
-    startTimer();
-    return () => {
-      clearInterval(interval.current);
-    };
+
+    return () => clearInterval(interval);
   });
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 font-kaushanScriptRegular">
-      <div className="bg-red-400 w-[33.8rem] h-[9.9rem] flex justify-between">
-        <div className="bg-slate-500 basis-28 flex flex-col justify-center items-center border-2 border-[#ffffffcf] ">
-          <span className="text-4xl mb-3">{timerDays}</span>
-          Days
+    <main className="flex flex-col items-center justify-between">
+      {partyTime ? (
+        <h1>Happy new year</h1>
+      ) : (
+        <div className="w-[33.8rem] h-[9.9rem] flex justify-between">
+          <div className="bg-slate-500 basis-32 flex flex-col justify-center items-center border-2 border-[#ffffffcf] ">
+            <span className="text-4xl mb-3">{timerDays}</span>
+            Days
+          </div>
+          <div className="bg-slate-500 basis-32 flex flex-col justify-center items-center border-2 border-[#ffffffcf] ">
+            <span className="text-4xl mb-3">{timerHours}</span>
+            Hours
+          </div>
+          <div className="bg-slate-500 basis-32 flex flex-col justify-center items-center border-2 border-[#ffffffcf] ">
+            <span className="text-4xl mb-3">{timerMinutes}</span>
+            Minutes
+          </div>
+          <div className="bg-slate-500 basis-32 flex flex-col justify-center items-center border-2 border-[#ffffffcf] ">
+            <span className="text-4xl mb-3">{timerSeconds}</span>
+            Seconds
+          </div>
         </div>
-        <div className="bg-slate-500 basis-28 flex flex-col justify-center items-center border-2 border-[#ffffffcf] ">
-          <span className="text-4xl mb-3">{timerHours}</span>
-          Hours
-        </div>
-        <div className="bg-slate-500 basis-28 flex flex-col justify-center items-center border-2 border-[#ffffffcf] ">
-          <span className="text-4xl mb-3">{timerMinutes}</span>
-          Minutes
-        </div>
-        <div className="bg-slate-500 basis-28 flex flex-col justify-center items-center border-2 border-[#ffffffcf] ">
-          <span className="text-4xl mb-3">{timerSeconds}</span>
-          Seconds
+      )}
+
+      <div className="w-full h-[54rem] relative bgImg p-14">
+        {/* <div className="absolute w-full h-[54rem]">
+          <Image
+            src="/assets/images/bg-006.jpg"
+            fill
+            // width={300}
+            // height={500}
+            alt="image"
+          />
+        </div> */}
+        <div className="absolute text-white flex flex-col justify-center items-center">
+          <Image
+            src="/assets/images/headphone-01.png"
+            width={100}
+            height={100}
+          />
+          <h3 className="text-[#FF4955]">Up to 50% Off</h3>
+          <h2>Grab Your Favorites Before They're Gone</h2>
+          <p className="text-center w-full">
+            You can trust us to bring you the latest technology at unbeatable
+            prices. Don’t miss this limited-time opportunity to upgrade your
+            audio game. Grab your perfect pair now!
+          </p>
         </div>
       </div>
     </main>
