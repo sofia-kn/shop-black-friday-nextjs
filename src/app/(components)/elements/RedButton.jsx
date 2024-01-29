8b7846e33b8154cd71fb812f49d3965278ca69cf
Sofia Katiran <sofiakatiran@gmail.com>
2024-01-29 09:35:10 +0330

    feat(TimeOffer): completed limited time offer section

--- src/app/(components)/elements/RedButton.jsx
+++ src/app/(components)/elements/RedButton.jsx
@@ -3,8 +3,8 @@ import React from "react";
 export default function RedButton({textCenter, textLeft}) {
   return (
     <div className={`text-${textCenter} lg:text-${textLeft}`}>
-      <button className="bg-[#fc3746] pt-3 pb-3 pl-5 pr-5 font-Lr">
-        <a className="text-lg tracking-[2px]">SHOP NOW</a>
+      <button className="bg-[#fc3746] py-4 px-8 font-Lr">
+        <a className="text-[2rem] tracking-[2px]">SHOP NOW</a>
       </button>
     </div>
   );
