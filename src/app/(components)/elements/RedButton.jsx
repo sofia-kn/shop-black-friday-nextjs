c26f0232cc2822d5f7529904f8e8815fe620b0c4
Sofia Katiran <sofiakatiran@gmail.com>
2024-01-29 11:10:22 +0330

    feat(BlackFridayMadness): created BlackFridayMadness component

--- src/app/(components)/elements/RedButton.jsx
+++ src/app/(components)/elements/RedButton.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function RedButton({textCenter, textLeft}) {
   return (
     <div className={`text-${textCenter} lg:text-${textLeft}`}>
-      <button className="bg-[#fc3746] py-4 px-8 font-Lr">
+      <button className="bg-[#fc3746] py-3 px-4 lg:py-4 lg:px-8 font-Lr">
         <a className="text-[2rem] tracking-[2px]">SHOP NOW</a>
       </button>
     </div>
---
+++ src/app/(components)/home/BlackFridayMadness.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import ProductCard from "../elements/ProductCard";
+export default function BlackFridayMadness() {
+  return (
+    <>
+      <div className="flex flex-col">
+        <h4 className="text-[#FF4955] text-[2rem] lg:text-[2.5rem] text-center font-Kr">
+          Unbelievable Discounts Await You
+        </h4>
+        <h2 className="text-[3.3rem] md:text-[4rem] lg:text-[6rem] xl:text-[8rem] font-medium text-center font-Lr sm:mt-3 md:mt-0">
+          BLACK FRIDAY MADNESS IS HERE!
+        </h2>
+        <span className="w-[60px] bg-white border-t-[0.7px] my-0 mx-auto mt-[2rem] md:mt-[1rem]"></span>
+      </div>
+      <div className="w-full grid gap-6 grid-cols-2 grid-rows-2 md:grid-cols-3 lg:grid-cols-4 lg:grid-rows-1 px-5 mr-2 md:px-7  lg-max:px-[74px] 2xl-max:px-[90px] mt-[3rem]">
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+      </div>
+    </>
+  );
+}
