ebbfd85ccf27f552a4001c4f2f1a9081934a0fa5
Sofia Katiran <sofiakatiran@gmail.com>
2024-02-16 12:56:30 +0330

    feat(Footer): completed responsive

--- src/app/components/footer/Footer.tsx
+++ src/app/components/footer/Footer.tsx
@@ -2,21 +2,21 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <div className="border-t-[0.5px] text-center px-[4rem] py-[4rem] border-gray-600 font-M">
+    <div className="flex flex-col lg:flex-row justify-center lg:justify-between items-center border-t-[0.5px] text-center px-[4rem] py-[4rem] lg:px-[3rem] lg:py-[3rem] xl:px-[5rem] xl:py-8 border-gray-600 font-M">
       <p className="text-2xl text-slate-300">
         Copyright © 2024 Black Friday | Powered by Black Friday
       </p>
-      <div className="flex flex-col justify-center items-center mt-8">
-        <div className="cursor-pointer hover:bg-black hover:text-red-600 items-center text-3xl text-slate-300">
+      <div className="flex flex-col justify-center items-center mt-8 xl:flex-row xl:mt-0">
+        <div className="cursor-pointer hover:bg-black hover:text-red-600 items-center text-3xl text-slate-300 xl:px-[1rem] xl:text-4xl">
           <Link href="#">HOME</Link>
         </div>
-        <div className="cursor-pointer hover:bg-black hover:text-red-600 items-center text-3xl text-slate-300">
+        <div className="cursor-pointer hover:bg-black hover:text-red-600 items-center text-3xl text-slate-300 xl:px-[1rem] xl:text-4xl">
           <Link href="#">SHOP</Link>
         </div>
-        <div className="cursor-pointer hover:bg-black hover:text-red-600 items-center text-3xl text-slate-300">
+        <div className="cursor-pointer hover:bg-black hover:text-red-600 items-center text-3xl text-slate-300 xl:px-[1rem] xl:text-4xl">
           <Link href="#">ABOUT</Link>
         </div>
-        <div className="cursor-pointer hover:bg-black hover:text-red-600 items-center text-3xl text-slate-300">
+        <div className="cursor-pointer hover:bg-black hover:text-red-600 items-center text-3xl text-slate-300 xl:px-[1rem] xl:text-4xl">
           <Link href="#">CONTACT</Link>
         </div>
       </div>
