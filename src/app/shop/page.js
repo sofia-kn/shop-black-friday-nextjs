22f39400e34369d8d102f6c8c9ceaf8e8a1b1eaf
Sofia Katiran <sofiakatiran@gmail.com>
2024-02-29 12:51:05 +0330

    feat(PaginationControlsProps): styled pagination buttons

--- src/app/shop/page.js
+++ src/app/shop/page.js
@@ -3,7 +3,27 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var react_1 = require("react");
 var NavigateNext_1 = require("@mui/icons-material/NavigateNext");
 var ProductCard_1 = require("../components/elements/ProductCard");
-function page() {
+var PaginationControls_1 = require("../components/elements/PaginationControls");
+var data = [
+    "entry 1",
+    "entry 2",
+    "entry 3",
+    "entry 4",
+    "entry 5",
+    "entry 6",
+    "entry 7",
+    "entry 8",
+    "entry 9",
+    "entry 10",
+];
+function page(_a) {
+    var _b, _c;
+    var searchParams = _a.searchParams;
+    var page = (_b = searchParams["page"]) !== null && _b !== void 0 ? _b : "1";
+    var per_page = (_c = searchParams["per_page"]) !== null && _c !== void 0 ? _c : "5";
+    var start = (Number(page) - 1) * Number(per_page); //0, 5, 10....
+    var end = start + Number(per_page); //5, 10, 15....
+    var entries = data.slice(start, end);
     return (<>
       <div className="absolute bottom-[50%] top-[10%] flex w-full flex-col justify-center items-center  px-2">
         <h1 className="text-[5.6rem] lg:text-[10rem] lg-max:text-[11rem]  leading-[5.1rem] lg:leading-[9rem] lg-max:leading-[10rem] tracking-[0] font-Lr font-normal uppercase ">
@@ -20,17 +40,24 @@ function page() {
 
       <main className="flex flex-col items-center justify-between relative lg:mt-[5rem] customContainer">
         <div className=" mt-[-38rem]">
-        <span className="text-[1.5rem] text-gray-300">Showing 1–8 of 16 results</span>
-         <div className="grid grid-cols-2 gap-8 mt-[3rem]">
-          <ProductCard_1.default />
-          <ProductCard_1.default />
-          <ProductCard_1.default />
-          <ProductCard_1.default />
-          <ProductCard_1.default />
-          <ProductCard_1.default />
-          <ProductCard_1.default />
-          <ProductCard_1.default />
-         </div>
+          <span className="text-[1.5rem] text-gray-300">
+            Showing 1–8 of 16 results
+          </span>
+          <div className="grid grid-cols-2 gap-8 mt-[3rem]">
+            <ProductCard_1.default />
+            <ProductCard_1.default />
+            <ProductCard_1.default />
+            <ProductCard_1.default />
+            <ProductCard_1.default />
+            <ProductCard_1.default />
+            <ProductCard_1.default />
+            <ProductCard_1.default />
+          </div>
+          <div className="flex flex-col gap-2 items-center text-[2rem]">
+            {entries.map(function (entry) { return (<p key={entry}>{entry}</p>); })}
+
+            <PaginationControls_1.default hasNextPage={end < data.length} hasPrevPage={start > 0}/>
+          </div>
         </div>
       </main>
     </>);
--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -1,8 +1,31 @@
 import React from "react";
 import NavigateNextIcon from "@mui/icons-material/NavigateNext";
 import ProductCard from "../components/elements/ProductCard";
+import PaginationControls from "../components/elements/PaginationControls";
+const data = [
+  "entry 1",
+  "entry 2",
+  "entry 3",
+  "entry 4",
+  "entry 5",
+  "entry 6",
+  "entry 7",
+  "entry 8",
+  "entry 9",
+  "entry 10",
+];
 
-export default function page() {
+export default function page({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | string[] | undefined };
+}) {
+  const page = searchParams["page"] ?? "1";
+  const per_page = searchParams["per_page"] ?? "5";
+  const start = (Number(page) - 1) * Number(per_page); //0, 5, 10....
+  const end = start + Number(per_page); //5, 10, 15....
+
+  const entries = data.slice(start, end);
   return (
     <>
       <div className="absolute bottom-[50%] top-[10%] flex w-full flex-col justify-center items-center  px-2">
@@ -20,17 +43,29 @@ export default function page() {
 
       <main className="flex flex-col items-center justify-between relative lg:mt-[5rem] customContainer">
         <div className=" mt-[-38rem]">
-        <span className="text-[1.5rem] text-gray-300">Showing 1–8 of 16 results</span>
-         <div className="grid grid-cols-2 gap-8 mt-[3rem]">
-          <ProductCard/>
-          <ProductCard/>
-          <ProductCard/>
-          <ProductCard/>
-          <ProductCard/>
-          <ProductCard/>
-          <ProductCard/>
-          <ProductCard/>
-         </div>
+          <span className="text-[1.5rem] text-gray-300">
+            Showing 1–8 of 16 results
+          </span>
+          <div className="grid grid-cols-2 gap-8 mt-[3rem]">
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+          </div>
+          <div className="flex flex-col gap-2 items-center text-[2rem]">
+            {entries.map((entry) => (
+              <p key={entry}>{entry}</p>
+            ))}
+
+            <PaginationControls
+              hasNextPage={end < data.length}
+              hasPrevPage={start > 0}
+            />
+          </div>
         </div>
       </main>
     </>
